08875bce9da75aae2dfb2c6ad7f5ec4f9ed66c7b
Daniel Moran <dijumx@gmail.com>
2016-09-17 13:14:56 +0100

    Add basic functionality and the first few tests

--- src/numberChains.c
+++ src/numberChains.c
@@ -13,14 +13,45 @@
 
 #include "constants.h"
 
+int extractTarget(char * target)
+{
+    char *c = target;
+    int res = 0;
 
+    /* Check for characters, we only want digits */
+    while (*c != '\0') {
+        if (!(*c >= '0' && *c <= '9')) {
+            return -1;
+        }
+        c++;
+    }
+    
+    /* Convert digits to an integer */
+    res = atoi(target);
+
+    return res;
+}
 
 int main(int argc, char * argv[])
 {
+    int target = 0;
+    int countAnd = 0;
+
     if (argc == 1) {
         fputs("Not enough arguments\n", stderr);
         return -1;
+    }else if (argc > 3) {
+        fputs("Too many arguments\n", stderr);
+        return -1;
     }
+
+    target = extractTarget(argv[1]);
+    if (target < 0){
+        fputs("Error processing target. Negatives and non-digits not allowed", stderr);
+        return -1;
+    }
+
+    printf("Number: %d, Length: %d\n", target, 1);
 
     return 0;
 }
